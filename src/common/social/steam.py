5c067b7fa50671467d82ea57b53f0ccf743f9ae7
Alex Siryi <desertkun@gmail.com>
2017-01-21 14:56:15 +0200

    Updated steam API

--- src/common/social/steam.py
+++ src/common/social/steam.py
@@ -37,18 +37,6 @@ class SteamAPI(common.social.SocialNetworkAPI):
                 e.code,
                 e.response.body if hasattr(e.response, "body") else str(e))
         else:
-            response_object = ujson.loads(response.body)
-
-            if "response" not in response_object:
-                raise common.social.APIError(500, "Steam error: no response field")
-
-            response = response_object["response"]
-
-            if "errorcode" in response:
-                raise common.social.APIError(
-                    400, "Steam error: " + str(response["errorcode"]) + " " + response["errordesc"])
-
-
             if "params" not in response:
                 raise common.social.APIError(500, "Steam error: no response/params field")
 
@@ -66,7 +54,7 @@ class SteamAPI(common.social.SocialNetworkAPI):
             raise Return(result)
 
     @coroutine
-    def api_get_user_info(self, username=None, key=None):
+    def api_get_user_info(self, username=None, key=None, env=None):
         try:
             response = yield self.api_get(
                 "ISteamUser/GetPlayerSummaries",
@@ -78,23 +66,63 @@ class SteamAPI(common.social.SocialNetworkAPI):
         except tornado.httpclient.HTTPError as e:
             raise common.social.APIError(e.code, e.response.body)
         else:
-            data = ujson.loads(response.body)
             try:
-                data = data["response"]["players"][0]
+                data = response["players"][0]
             except KeyError:
                 raise common.social.APIError(500, "Steam error: bad user info response")
 
-            raise Return(SteamAPI.process_user_info(data))
+            main_data = SteamAPI.process_user_info(data)
+
+            try:
+                kwargs = {
+                    "key": key,
+                    "steamid": username
+                }
+
+                if env and "ip_address" in env:
+                    kwargs["ipaddress"] = env["ip_address"]
+
+                response = yield self.api_get(
+                    "ISteamMicroTxn/GetUserInfo",
+                    {},
+                    v="v0001",
+                    **kwargs)
+
+            except tornado.httpclient.HTTPError as e:
+                pass
+            else:
+                if "params" in response:
+                    params = response["params"]
+
+                    main_data.update(SteamAPI.process_user_payment_info(params))
+
+            raise Return(main_data)
 
     @coroutine
     def api_get(self, operation, fields, v="v1", **kwargs):
 
         fields.update(**kwargs)
+
         result = yield self.client.fetch(
             SteamAPI.STEAM_API + "/" + operation + "/" + v + "?" +
             urllib.urlencode(fields))
 
-        raise Return(result)
+        try:
+            response_object = ujson.loads(result.body)
+        except (KeyError, ValueError):
+            raise common.social.APIError(500, "Corrupted steam response")
+
+        if "response" not in response_object:
+            raise common.social.APIError(500, "Steam error: no response field")
+
+        response = response_object["response"]
+
+        if "error" in response:
+            error = response["error"]
+            raise common.social.APIError(
+                400, "Steam error: " + str(error["errorcode"]) + " " + error["errordesc"])
+
+        raise Return(response)
 
     @coroutine
     def api_post(self, operation, fields, v="v1", **kwargs):
@@ -113,6 +141,14 @@ class SteamAPI(common.social.SocialNetworkAPI):
             "name": data["personaname"],
             "avatar": data["avatarmedium"],
             "profile": data["profileurl"]
+        }
+
+    @staticmethod
+    def process_user_payment_info(data):
+        return {
+            "currency": data.get("currency", "USD"),
+            "country": data.get("country", "unknown"),
+            "steam_status": data.get("status", "unknown")
         }
 
     def new_private_key(self, data):
