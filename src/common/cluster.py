a27f522f45683902887f596cdb353525428c6809
Alex Siryi <desertkun@gmail.com>
2017-07-05 20:29:58 +0300

    Added option to leave cluster

--- src/common/cluster.py
+++ src/common/cluster.py
@@ -16,21 +16,75 @@ class NoClusterError(Exception):
 
 
 class Cluster(object):
+    """
+    This object manages abstract cluster of players around some entity.
+    This is useful when you would like to have a lot of players on same entity, but with each player
+        attached to his tight group cluster (for example, 50 of them).
+
+    A nice example would be Cluster Leaderboards. When you post to a cluster leaderboard you can only see
+        records from your virtual group (cluster), and compete around them. Instead of having to compete
+        around hundreds of thousands of players you have only 50 instead, virtually.
+
+    See get_cluster for more info.
+    """
     def __init__(self, db, table_name, accounts_table_name):
+        """
+        :param db: A database reference
+        :param table_name: table name holding clusters themselves
+
+        Scheme for it would be:
+
+            CREATE TABLE `<table_name>` (
+                `cluster_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                `gamespace_id` int(11) unsigned NOT NULL,
+                `cluster_size` int(11) unsigned NOT NULL,
+                `cluster_data` int(11) unsigned NOT NULL,
+                PRIMARY KEY (`cluster_id`),
+                KEY `leaderboard_id` (`cluster_data`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+        :param accounts_table_name: table name holding cluster record (which player belong to whom)
+
+        According scheme for it (please note a reference to <table_name>):
+
+            CREATE TABLE `<accounts_table_name>` (
+                `account_id` int(11) unsigned NOT NULL,
+                `gamespace_id` int(11) unsigned NOT NULL,
+                `cluster_id` int(11) unsigned NOT NULL,
+                `cluster_data` int(11) unsigned NOT NULL,
+                PRIMARY KEY (`account_id`,`gamespace_id`,`cluster_id`,`cluster_data`),
+                UNIQUE KEY `account_id` (`account_id`,`gamespace_id`,`cluster_id`),
+                KEY `cluster_id` (`cluster_id`),
+                KEY `account_id_2` (`account_id`),
+                KEY `cluster_id_2` (`cluster_id`),
+                CONSTRAINT `leaderboard_cluster_accounts_ibfk_1`
+                    FOREIGN KEY (`cluster_id`) REFERENCES `<table_name>` (`cluster_id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        """
+
         self.db = db
         self.table_name = table_name
         self.accounts_table_name = accounts_table_name
 
     @coroutine
-    def delete_clusters(self, gamespace, key, db=None):
-        yield (db or self.db).execute(
+    def delete_clusters(self, gamespace, key):
+        """
+        Deletes all cluster records around some key
+        """
+        with (yield self.db.acquire()) as db:
+            yield self.delete_clusters(gamespace, key, db)
+
+    @coroutine
+    def delete_clusters_db(self, gamespace, key, db):
+        yield db.execute(
             """
                 DELETE FROM `{0}`
                 WHERE `gamespace_id`=%s AND `cluster_data`=%s;
             """.format(self.accounts_table_name),
             gamespace, key)
 
-        yield (db or self.db).execute(
+        yield db.execute(
             """
                 DELETE FROM `{0}`
                 WHERE `gamespace_id`=%s AND `cluster_data`=%s;
@@ -39,6 +93,9 @@ class Cluster(object):
 
     @coroutine
     def list_clusters(self, gamespace, key, db=None):
+        """
+        Returns list of cluster ids for certain key
+        """
         try:
             clusters = yield (db or self.db).query(
                 """
@@ -54,7 +111,76 @@ class Cluster(object):
         raise Return([cluster["cluster_id"] for cluster in clusters])
 
     @coroutine
-    def get_cluster(self, gamespace, account, key, cluster_size, auto_create=True):
+    def leave_cluster(self, gamespace, account, key, add_vacant_place=True):
+        """
+        Leaves a cluster for a player
+        :param gamespace: A gamespace
+        :param account: Player's account
+        :param key: A key of cluster to leave
+        :param add_vacant_place: if true, a new vacant space for a new player would be created
+        :return:
+        """
+
+        with (yield self.db.acquire(auto_commit=False)) as db:
+            try:
+
+                query = """
+                    SELECT `{0}`.`cluster_id`, `{1}`.`cluster_size`
+                    FROM `{0}`, `{1}`
+                    WHERE `{0}`.`gamespace_id`=%s AND `{0}`.`account_id`=%s AND `{0}`.`cluster_data`=%s AND
+                        `{1}`.`gamespace_id`=`{0}`.`gamespace_id` AND `{1}`.`cluster_id`=`{0}`.`cluster_id` AND
+                        `{1}`.`cluster_data`=`{0}`.`cluster_data`
+                    LIMIT 1
+                    FOR UPDATE;
+                """.format(self.accounts_table_name, self.table_name)
+
+                # look for existent join
+                cluster = yield db.get(query, gamespace, account, key)
+
+                if not cluster:
+                    return
+
+                cluster_size = cluster["cluster_size"]
+                cluster_id = cluster["cluster_id"]
+
+                yield db.execute(
+                    """
+                        DELETE FROM `{0}`
+                        WHERE `{0}`.`gamespace_id`=%s AND `{0}`.`cluster_id`=%s AND `{0}`.`account_id`=%s
+                    """.format(self.accounts_table_name),
+                    gamespace, cluster_id, account
+                )
+
+                if add_vacant_place:
+                    updated_cluster_size = cluster_size + 1
+
+                    yield db.execute(
+                        """
+                            UPDATE `{0}`
+                            SET `cluster_size`=%s
+                            WHERE `{0}`.`gamespace_id`=%s AND `{0}`.`cluster_id`=%s
+                        """.format(self.table_name),
+                        updated_cluster_size, gamespace, cluster_id
+                    )
+
+            except DatabaseError as e:
+                raise ClusterError("Failed to leave account cluster: " + e.args[1])
+            finally:
+                yield db.commit()
+
+    @coroutine
+    def get_cluster(self, gamespace, account, key, auto_create=True, cluster_size=50):
+        """
+        Acquires a cluster for a player over a certain key
+
+        :param gamespace: A gamespace
+        :param account: Player's account
+        :param key: A key to retrieve a cluster about
+        :param auto_create: Spawn a new cluster if there's no a free one
+        :param cluster_size: A cluster size for new cluster in case there's no a free one and auto_create is True
+        :return: Id of acquired cluster for player for a key
+        """
+
         try:
             # look for existent join
             cluster = yield self.db.get(
